f2d3b1efc29b573dd7f6143b34252202dc31f875
Hafeez Syed <feeziman007@hotmail.com>
2019-01-10 23:18:08 +1100

    fix styles

---
+++ build/precache-manifest.785ec5c11b840573263c194a72ca200e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2912c546f1294bf5d9da",
+    "url": "/reduxstagram/static/js/runtime~main.2912c546.js"
+  },
+  {
+    "revision": "702f6b9c81934473d6c3",
+    "url": "/reduxstagram/static/js/main.702f6b9c.chunk.js"
+  },
+  {
+    "revision": "c1d8b6090245c168c73c",
+    "url": "/reduxstagram/static/js/1.c1d8b609.chunk.js"
+  },
+  {
+    "revision": "702f6b9c81934473d6c3",
+    "url": "/reduxstagram/static/css/main.f843279b.chunk.css"
+  },
+  {
+    "revision": "b9b431fe88b85c34b4a26ed37204d0aa",
+    "url": "/reduxstagram/index.html"
+  }
+];
